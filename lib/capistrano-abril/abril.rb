d4a72a6d1214054f7f1623ae90d8ae9cede736d8
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2011-04-07 10:08:00 -0300

    abril.rb: vars inside namespace. Trigger for setup_vars.
    Using cleanup and cleanup_shared.

--- lib/capistrano-abril/abril.rb
+++ lib/capistrano-abril/abril.rb
@@ -6,66 +6,100 @@ CapistranoAbril.with_configuration do
     Time.now.strftime("%Y-%m-%d-%H%M")
   end
 
-  # Abril defaults
-  set :release_name  , _mytime()
-  set :ugroup        , "infra"
-  set :umask         , "002"
-
-  set :keep_releases , 5
-  set :use_sudo      , false
-
-  set :base_path     , "/abd"
-  set :app_path      , "#{base_path}/app"
-  set :logs_path     , "#{base_path}/logs/#{application}"
-
-  set :deploy_to     , "#{app_path}/#{application}"
-  set :current_path  , "#{deploy_to}/current"
-  set :releases_path , "#{deploy_to}/releases"
-  set :shared_path   , "#{deploy_to}"
-  set :config_path   , "#{deploy_to}/config"
-  set :bundle_path   , "#{deploy_to}/bundle"
-
-  set :default_shell , "/bin/bash"
-
-
-# set :base_path     , fetch(:base_path    , "/abd"                     )
-# set :app_path      , fetch(:app_path     ,"#{base_path}/app"          )
-# set :logs_path     , fetch(:logs_path    ,"#{base_path}/logs/#{application}")
-
-# set :deploy_to     , fetch(:deploy_to    ,"#{app_path}/#{application}")
-# set :current_path  , fetch(:current_path ,"#{deploy_to}/current"      )
-# set :releases_path , fetch(:releases_path,"#{deploy_to}/releases"     )
-# set :shared_path   , fetch(:shared_path  ,"#{deploy_to}"              )
-# set :config_path   , fetch(:config_path  ,"#{deploy_to}/config"       )
-# set :bundle_path   , fetch(:bundle_path  ,"#{deploy_to}/bundle"       )
-
-
   namespace :deploy do
 
-    desc "[internal] Clean up unused dirs."
-    task :cleanup_shared do
+    ### setup_vars
+    desc "(abril.rb) [internal] Setting up vars"
+    task :setup_vars, :roles => :app, :except => { :no_release =>  true } do
+
+        # Abril defaults
+        set :release_name  , _mytime()
+        set :ugroup        , "infra"
+        set :umask         , "002"
+
+        set :keep_releases , 5
+        set :use_sudo      , false
+
+        set :base_path     , "/abd"
+        set :app_path      , "#{base_path}/app"
+        set :logs_path     , "#{base_path}/logs/#{application}"
+
+        set :deploy_to     , "#{app_path}/#{application}"
+        set :current_path  , "#{deploy_to}/current"
+        set :releases_path , "#{deploy_to}/releases"
+        set :shared_path   , "#{deploy_to}"
+        set :config_path   , "#{deploy_to}/config"
+        set :bundle_path   , "#{deploy_to}/bundle"
+        set :structure_path, "#{deploy_to}/structure"
+
+        set :default_shell , "/bin/bash"
+
+      # set :base_path     , fetch(:base_path    , "/abd"                     )
+      # set :app_path      , fetch(:app_path     ,"#{base_path}/app"          )
+      # set :logs_path     , fetch(:logs_path    ,"#{base_path}/logs/#{application}")
+      # set :deploy_to     , fetch(:deploy_to    ,"#{app_path}/#{application}")
+      # set :current_path  , fetch(:current_path ,"#{deploy_to}/current"      )
+      # set :releases_path , fetch(:releases_path,"#{deploy_to}/releases"     )
+      # set :shared_path   , fetch(:shared_path  ,"#{deploy_to}"              )
+      # set :config_path   , fetch(:config_path  ,"#{deploy_to}/config"       )
+      # set :bundle_path   , fetch(:bundle_path  ,"#{deploy_to}/bundle"       )
+
+    end
+    before  "deploy:update" , "deploy:setup_vars"
+
+    ### show_vars
+    desc "(abril.rb) show_vars: Show custom variables defined by abril.rb."
+    task :show_vars , :roles => :app, :except => { :no_release =>  true } do
+
+        deploy.setup_vars
+
+        puts "-" * 70
+        puts "abril.rb:show_vars - using current values:"
+        puts ""
+        puts "              port: #{port}"
+        puts "             umask: #{umask}"
+        puts "              user: #{user}"
+        puts "       application: #{application}"
+        puts "        repository: #{repository}"
+        puts "  repository_cache: #{repository_cache}"
+        puts ""
+        puts "         base_path: #{base_path}"
+        puts "          app_path: #{app_path}"
+        puts "       deploy_path: #{deploy_to}"
+        puts "      current_path: #{current_path}"
+        puts "     releases_path: #{releases_path}"
+        puts "       shared_path: #{shared_path}"
+        puts "       config_path: #{config_path}"
+        puts "    structure_path: #{structure_path}"
+        puts "         logs_path: #{logs_path}"
+        puts "-" * 70
+        puts ""
+
+    end
+
+    ### deploy:start/stop/restart
+    [:start, :stop, :restart, :status].each do |t|
+        desc "(abril.rb) sudo apachectl #{t}"
+        task t, :roles => :app, :except => { :no_release => true } do
+          run "sudo apachectl #{t}"
+        end
+    end
+
+    ### cleanup_shared
+    desc "(abril.rb) [internal] Clean up unused dirs."
+    task :cleanup_shared, :roles => :app, :except => { :no_release => true } do
         run "/bin/rm -rf #{shared_path}/{log,pids,system} #{latest_release}/public/system"
         run "ln -nsf #{logs_path} #{latest_release}/log      "
         run "ln -nsf #{logs_path} #{latest_release}/tmp/pids "
-      end
-
+    end
     after  "deploy:finalize_update" , "deploy:cleanup_shared"
 
-    desc "OVR"
-    task :start do
-    end
+    ### cleanup releases_path
+    after  "deploy:finalize_update" , "deploy:cleanup"
 
-    desc "OVR"
-    task :stop do
-    end
+    ###
 
-    desc "OVR"
-    task :restart do
-    end
+  end # namespace
 
-  end
+end # CapistranoAbril
 
-
-# set :group_writable, fetch(:group_writable, true )
-
-end
