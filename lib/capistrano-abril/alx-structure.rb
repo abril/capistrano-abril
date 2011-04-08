c44abfad44b53c222e77db7c0772265fde38b07c
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2011-04-08 20:00:19 -0300

    New: alx-structure

--- lib/capistrano-abril/alx-structure.rb
+++ lib/capistrano-abril/alx-structure.rb
@@ -4,11 +4,8 @@ CapistranoAbril.with_configuration do
 
   namespace :deploy do
 
-    desc '(site-reference.rb) Alexandria site-reference deploy.'
+    desc '(alx-structure.rb) Alexandria site-reference deploy.'
     task :structure do
-
-      # pre-req: abril.rb
-      deploy.setup_vars
 
       # checking....
       [ :structure_path, :structure_repos, :structure_branch ].each do |item|
@@ -21,20 +18,22 @@ CapistranoAbril.with_configuration do
       set :st_branch, ENV['TAG'] || ENV['BRANCH'] || :structure_branch || 'master'
 
       # structure clone
-      run "[ -d #{structure_path} ] && /bin/rm -rf #{structure_path}"
-      run "git clone --depth 1 #{structure_repos} #{structure_path} -b #{st_branch}"
-
       # symlink inside site-reference
-      run "ln -nsf #{structure_path} #{latest_release}/structure"
+      run <<-CMD
+        if [ ! -d #{structure_path}/.git ] ;
+        then echo "Cloning structure:"   &&
+             git clone --depth 1 #{structure_repos} #{structure_path} &&
+             cd #{structure_path} && git checkout -b #{st_branch}     ;
+        else echo "Resetting structure:" &&
+             cd #{structure_path} && git reset --hard && git clean -d -x -f;
+        fi ;
+        ln -nsf #{structure_path} #{latest_release}/structure
+      CMD
 
     end
+    after "deploy:symlink" , "deploy:structure"
 
   end # namespace
 
 end
-
-#     run "mkdir -p #{structure_path} && chmod g+w #{structure_path}"
-#     run "rm -rf #{latest_release}/structure && ln -nsf  #{structure_path}/current #{latest_release}/structure"
-
-#     run "rm -rf #{latest_release}/structure && ln -nsf #{structure_path} #{latest_release}/structure"
 
