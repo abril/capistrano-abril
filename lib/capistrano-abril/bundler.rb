4a75bc6c66994d4bdbb9229680677f5e40d00900
Lucas Fais <lucas.fais@abril.com.br>
2012-02-01 16:51:56 -0200

    Changed way to find current environment before run bundler task. Useful for non-rails applications.

--- lib/capistrano-abril/bundler.rb
+++ lib/capistrano-abril/bundler.rb
@@ -8,16 +8,15 @@ CapistranoAbril.with_configuration do
     task :install do
 
       run "mkdir -p #{shared_path}/bundle"
-#
-
-
-      if "#{rails_env}" != 'production'
-#    if 1 != 0
-          # dev/qa
-          run "bundle install --gemfile #{latest_release}/Gemfile --path #{shared_path}/bundle "
+      
+      env = fetch(:rails_env, fetch(:env, 'production'))
+      
+      if ['production', "stage"].include?(env)
+        # stage/prod
+        run "bundle install --gemfile #{latest_release}/Gemfile --path #{shared_path}/bundle --deployment --without development test"
       else
-          # stage/prod
-          run "bundle install --gemfile #{latest_release}/Gemfile --path #{shared_path}/bundle --without development test"
+        # dev/qa
+        run "bundle install --gemfile #{latest_release}/Gemfile --path #{shared_path}/bundle --deployment"
       end
 
     end
@@ -26,4 +25,3 @@ CapistranoAbril.with_configuration do
   end # namespace
 
 end
-
